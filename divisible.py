d9a69baf5202786f2add623b9e9d5e815441e667
tyralyn <tyralyntran@gmail.com>
2016-01-27 14:41:20 -0800

    adjusted to print entire list separated by commas, including enclosing brackets

--- divisible.py
+++ divisible.py
@@ -1,5 +1,7 @@
 def f():
-    for i in range (2000,3200):
-        if i % 7 == 0 and i % 3 != 0:
-            print (i)
-    return;
+	l=[]
+	for i in range (2000,3200):
+		if i % 7 == 0 and i % 3 != 0:
+			l.append(str(i))
+	print (l)      	
+	return;
