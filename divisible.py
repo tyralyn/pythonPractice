f5d5935011bede050b225ad9e6cf90b92baf5140
tyralyn <tyralyntran@gmail.com>
2016-01-27 14:44:02 -0800

    utilizes join method to print list with spaces and commas and no brackets

--- divisible.py
+++ divisible.py
@@ -1,7 +1,9 @@
 def f():
 	l=[]
+	comma = ", "
 	for i in range (2000,3200):
 		if i % 7 == 0 and i % 3 != 0:
 			l.append(str(i))
-	print (l)      	
+	print (comma.join(l)) #you must declare the comma string rather than just sticking the literal before the join -- why     	
 	return;
+
